f0945b26781acd014d657f1821b1c661e9f8232d
emirontop1 <dincemr@gmail.com>
2026-06-18 20:13:26 +0300

    Update Usage.lua

--- Usage.lua
+++ Usage.lua
@@ -1,28 +1,28 @@
 _G.Version = "V1" -- Değişken adı 'Version' olarak kaldı
 local EmoC = loadstring(game:HttpGet('https://raw.githubusercontent.com/emirontop1/EmoC/refs/heads/main/src/' .. _G.Version .. '.lua'))()
 
-local MyWindow = EmoC:CreateWindow("Wind UI Ultimate", "Gelişmiş Yönetim Paneli", {
+local MyWindow = EmoC:CreateWindow("EmoC Example", "Subtext", {
     icon = "shield-check", 
     minWidth = 400,        
     maxWidth = 800         
 })
 
-local HomeTab = MyWindow:CreateTab("Ana Sayfa", "Genel Ayarlar ve Durum", "home")
+local HomeTab = MyWindow:CreateTab("Random ahh tab", "Random ahh tab description", "home")
 
-HomeTab:CreateToggle("Otomatik Çiftlik", "Arka planda otomatik kasılmayı başlatır.", "box", false, false, function(state)
+HomeTab:CreateToggle("Another random ahh toggle", "Another random ahh toggle with description.", "box", false, false, function(state)
     print("Otomatik Çiftlik Durumu: ", state)
 end)
 
-HomeTab:CreateButton("Modülleri Yükle", "Gerekli tüm kütüphaneleri sisteme enjekte eder.", "play", function()
+HomeTab:CreateButton("example button", "Gerekli tüm kütüphaneleri sisteme enjekte eder.", "play", function()
     print("Modüller başarıyla yüklendi!")
 end)
 
-local SettingsTab = MyWindow:CreateTab("Ayarlar", "Arayüz ve Sistem Seçenekleri", "settings-gear")
+local SettingsTab = MyWindow:CreateTab("random ahh tab", "and one more random ahh Tab description.", "settings-gear")
 
-SettingsTab:CreateToggle("Performans Modu", "Gereksiz efektleri kapatarak FPS artırır.", "power", true, true, function(state)
+SettingsTab:CreateToggle("another random ahh toggle", "another random ahh toggle description.", "power", true, true, function(state)
     print("Performans Modu Aktif: ", state)
 end)
 
-SettingsTab:CreateButton("Kullanıcı Bilgisi", "Mevcut profil detaylarını konsola yazdırır.", "user", function()
+SettingsTab:CreateButton("Button example", "button description.", "user", function()
     print("Kullanıcı: " .. game.Players.LocalPlayer.Name)
 end)
